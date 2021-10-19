5b8c47da1f238c2b8331103de2a1e9f07274acfe
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-10-19 19:26:05 +0200

    clean code + cover img + box-shadow header

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -2,7 +2,7 @@ import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 import Home from '../pages/Home/Home';
 import Profile from '../pages/Profile/Profile';
-import { BrowserRouter, Redirect, Route, Switch, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { useContext } from 'react';
 import AuthContext from '../components/AppContext/Auth.context';
 import Friends from '../pages/Friends/Friends';
@@ -12,16 +12,10 @@ import Notifications from '../pages/Notifications/Notifications';
 import Settings from '../pages/Settings/Settings';
 import Remember from '../pages/Remember/Remember';
 
-interface MatchParams {
-    username: string;
-}
-
-interface MatchProps extends RouteComponentProps<MatchParams> {}
-
 const Router = () => {
     const { connexion } = useContext(AuthContext);
     const loggedIn = connexion.loggedIn;
-    const user = connexion.user!;
+    // const user = connexion.user!;
 
     return (
         <BrowserRouter>
@@ -31,25 +25,17 @@ const Router = () => {
                         <Topbar />
                         <Switch>
                             {/* Main menu links */}
-                            <Route exact path="/home" component={Home}></Route>
-                            <Route exact path="/friends/" component={Friends}></Route>
+                            <Route exact path="/home" component={Home} />
+                            <Route exact path="/friends/" component={Friends} />
 
                             {/* Second menu links */}
-                            {/* <Route
-                                exact
-                                path="/profile"
-                                render={() => (
-                                    <Profile username={user.username} />
-                                )}
-                            /> */}
                             <Route path="/profile/:username" component={Profile} />
-
-                            <Route exact path="/chat" component={Chat}></Route>
-                            <Route exact path="/notifications" component={Notifications}></Route>
-                            <Route exact path="/settings" component={Settings}></Route>
+                            <Route exact path="/chat" component={Chat} />
+                            <Route exact path="/notifications" component={Notifications} />
+                            <Route exact path="/settings" component={Settings} />
 
                             {/* Sidebar links */}
-                            <Route exact path="/remember" component={Remember}></Route>
+                            <Route exact path="/remember" component={Remember} />
 
                             <Redirect to="/home" />
                         </Switch>
